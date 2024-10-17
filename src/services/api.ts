6b48f00ec176f39f1728b93e4756f0ffe6d01eec
Christian Almli <chrisalm97@gmail.com>
2024-10-18 00:24:45 +0200

    store api in .env and add .env to .gitignore

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://v2.api.noroff.dev/holidaze',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
